5a02563e9ab7fd40444cb34b2ef3930b0491c284
Augusto <augusto_pedraza@hotmail.com>
2013-01-08 23:43:50 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheHappyLitteBook::Application.config.secret_token = '3c8773467b0f9471546ae2d6b5bc7c99c708be3179ffc75f9f22db9ce45eec04ad4addb2a692a4267d1e91bbd353392f507568e7e438c946e58b6654e9d301f7'
