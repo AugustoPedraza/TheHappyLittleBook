e0fac998da744b581fd789ad724f60fdd55fc896
Augusto <augusto_pedraza@hotmail.com>
2013-01-21 20:42:51 -0300

    [Add ActiveAdmin support]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 TheHappyLitteBook::Application.routes.draw do
-  resources :books
+  ActiveAdmin.routes(self)
+  devise_for :users, ActiveAdmin::Devise.config
 
+  resources :books
 
   authenticated :user do
     root :to => 'home#index'
   end
+
   root :to => "home#index"
   devise_for :users
   resources :users
